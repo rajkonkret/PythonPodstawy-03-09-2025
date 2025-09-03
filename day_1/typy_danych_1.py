ee34a8466bee26807f08b78f06c95fc2c527d502
rajkonkret <rajkonkret660@gmail.com>
2025-09-03 13:56:00 +0200

    [1 7]add typy_danych_1.py

---
+++ day_1/typy_danych_1.py
@@ -0,0 +1,34 @@
+wiek = 47  # int
+rok = 2025  # int
+temp = 36.6  # float, zmiennoprzecinkowe
+
+print(wiek + rok)  # 2072
+print(wiek - rok)  # -1978
+print(wiek * rok)  # 95175
+print(wiek / rok)  # 0.023209876543209877
+
+print(rok // wiek)  # 43, częśc całkowita z dzielenia
+print(rok % wiek)  # modulo, reszta z dzielenia
+
+print(10 % 3)  # restza 1
+
+print(wiek ** rok)  # potęgowanie
+# len() - długość
+# str() - zmieniamy na tekst i mierzymy długość
+print(len(str(wiek ** rok)))  # 3386
+# print(len(str(wiek ** rok ** 2)))
+# ValueError: Exceeds the limit (4300 digits)
+# for integer string conversion; use sys.set_int_max_str_digits() to increase the limit
+
+print(54 - 5 * 4 / 1 + 4 / 2)  # 36.0
+print(54 - 5 * (4 / 1 + 4) / 2)  # 34.0
+
+# float - liczby zmiennoprzecinkowe
+# posiadają bład zaookrąglenia
+print(0.2 + 0.8)  # 1.0
+print(0.2 + 0.7)  # 0.8999999999999999
+# For example, in a floating-point arithmetic with five base-ten digits,
+# the sum 12.345 + 1.0001 = 13.3451 might be rounded to 13.345.
+# decimal()  - pozwala ominąc problem zaokrąglenia
+
+
