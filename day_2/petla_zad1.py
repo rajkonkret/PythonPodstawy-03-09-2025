f33b3936b29fb99fccd563bbb5a2b3e47ddee0cb
rajkonkret <rajkonkret660@gmail.com>
2025-09-04 12:54:34 +0200

    [2 12a] add petle_zad1

--- day_2/petla_zad1.py
+++ day_2/petla_zad1.py
@@ -26,7 +26,33 @@ lista_wynik = []
 for _ in range(6):
     wyn = random.choice(lista_kule)
     lista_kule.remove(wyn)
-    print(wyn)
+    # print(wyn)
     lista_wynik.append(wyn)
 
 print(lista_wynik)  # [47, 33, 22, 44, 8, 18]
+
+for i in range(10):
+    if i % 2 == 0:  # modulo, reszta z dzielenia
+        print(i, "parzysta")
+# 0 parzysta
+# 2 parzysta
+# 4 parzysta
+# 6 parzysta
+# 8 parzysta
+
+# list comprehensions
+lista3 = [j for j in range(10) if j % 2 == 0]
+print(lista3)  # [0, 2, 4, 6, 8]
+
+for c in lista3:  # pobiera po kolei wszystkie eleemnty z lista3
+    if c > 4:
+        print(c, "Większe od 4")
+    elif c == 4:
+        print(c, "Rowne 4")
+    else:
+        print(c, "Mniejsze od 4")
+# 0 Mniejsze od 4
+# 2 Mniejsze od 4
+# 4 Rowne 4
+# 6 Większe od 4
+# 8 Większe od 4
