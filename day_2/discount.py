4b606e6d863849e696888ed98038bd3990764c74
rajkonkret <rajkonkret660@gmail.com>
2025-09-04 15:17:15 +0200

    [2 20] update discount.py

--- day_2/discount.py
+++ day_2/discount.py
@@ -28,3 +28,24 @@ print(formated_time)  # 15:03
 object_time = datetime.now().strptime("04/09/2025", "%d/%m/%Y")
 print(object_time)  # 2025-09-04 00:00:00
 print(type(object_time))  # <class 'datetime.datetime'>
+
+print(25 * "-")
+# -------------------------
+
+products = [
+    {"sku": 1, "exp_date": today, "price": 200},
+    {"sku": 2, "exp_date": today, "price": 100},
+    {"sku": 3, "exp_date": tomorrow, "price": 50},
+    {"sku": 4, "exp_date": today, "price": 199.99},
+    {"sku": 5, "exp_date": today, "price": 500},
+]
+
+for p in products:
+    print(p["exp_date"])
+    if p['exp_date'] != today:
+        continue  # wróc na początek pętli, nie wykonuj kolejnych komend
+    p['price'] *= 0.8  # p = p * 0.8
+    print("Zmiana ceny")
+    print(f"""
+Price for {p['sku']}
+is now {p['price']:.2f}""")  # .2f - zaokrąglenie do dwoch miejsc po przecinku
