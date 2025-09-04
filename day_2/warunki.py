80d2a6abae74213c1091b0a2635b9a584480c590
rajkonkret <rajkonkret660@gmail.com>
2025-09-04 11:01:08 +0200

    [2 7] update warunki.py

--- day_2/warunki.py
+++ day_2/warunki.py
@@ -51,3 +51,25 @@ if n > 3:
 # walrus operator, operator morsa
 if (n := len(a)) > 3:
     print(f'Długość wynosi {n}, więcej niz 3')
+
+# snake_case
+
+# kolejnosc ma znaczenie
+# pierwszy True powoduje wyjście z drzewka
+
+podatek = 0
+zarobki = int(input("Podaj zarobki"))
+if zarobki < 10000:
+    podatek = 0
+elif zarobki < 40000:  # zarobki >= 10000 and zarobki < 40000
+    podatek = 0.2
+elif zarobki < 100000:
+    podatek = 0.4
+# # nigdy by nie weszło w ten fragment warunku
+# elif zarobki < 40000:  # zarobki >= 10000 and zarobki < 40000
+#     podatek = 0.2
+else:
+    podatek = 0.9
+
+print(f"Podatek wynosi {podatek * zarobki} pln.")
+# podatek 0.2 dla przedziału 10000 do 39999
