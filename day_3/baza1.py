2be6ea18656b8ba1cea7ac326a62572798dadbc4
rajkonkret <rajkonkret660@gmail.com>
2025-09-05 14:33:37 +0200

    [3 16] update baza.py

--- day_3/baza1.py
+++ day_3/baza1.py
@@ -11,6 +11,28 @@ try:
     cursor = sql_connection.cursor()
     print('Baza została podłączona')
 
+    query = """
+    CREATE TABLE IF NOT EXISTS developers (
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    salary REAL NOT NULL);
+    """
+
+    # cursor.execute(query)
+    # sql_connection.commit()
+
+    insert = """
+    INSERT INTO developers(id,name,salary) VALUES (1, 'Radek', 10000);
+    """
+    # cursor.execute(insert)
+    # sql_connection.commit()
+
+    select = """
+    SELECT * FROM developers;
+    """
+    for row in cursor.execute(select):
+        print(row)  # (1, 'Radek', 10000.0)
+
 except sqlite3.Error as e:
     print("Błąd:", e)
 finally:
